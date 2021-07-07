9f1d5a6aa9c7f4216b20cce347047e9ed9c70e19
raytlty <alexmao930918@gmail.com>
2021-07-07 13:07:47 +0800

    deploy v3

--- scripts/deploy-rinkeby.ts
+++ scripts/deploy-rinkeby.ts
@@ -2,22 +2,13 @@ import { ethers } from 'hardhat';
 
 
 async function main() {
-    const YangViewFactory = await ethers.getContractFactory('YangView');
     const YangNFTFactory = await ethers.getContractFactory('YangNFTVault');
-    const YangView = await YangViewFactory.deploy();
-    await YangView.deployed();
     const YangNFT = await YangNFTFactory.deploy();
     await YangNFT.deployed();
 
-    console.log('YANGView:')
-    console.log(YangView.address) // 0x2Ba5310482a9Fbb242633d8a82027af41FB4B579
-    console.log(YangView.deployTransaction.hash);
-
     console.log('YANGNFTVault')
-    console.log(YangNFT.address) // 0xd1309B94DAcA28bc402694a60Aee53089cfff5E5
+    console.log(YangNFT.address) // 0xBfb7003F5c8375C7B64519CE4fb5a9BaE8023853
     console.log(YangNFT.deployTransaction.hash);
-
-    await YangNFT.setYangView(YangView.address);
 }
 
 main()
